07f45805dc718de7029fa4e2b1344aa3c6c599b5
Nataliia Valko <75252511+NataliiaValko@users.noreply.github.com>
2023-02-21 11:25:48 +0200

    adds normalize

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,11 @@
 export function fetchCountries(name){
-    const URL = `https://restcountries.com/v2/name/${name}`;
+    // const URL = `https://restcountries.com/v2/name/${name}`;
 
-    return fetch(
-        `${URL}?fields=name,capital,flags,population,languages`
-    ).then (response => {
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-    }).json()
+    // return fetch(
+    //     `${URL}?fields=name,capital,flags,population,languages`
+    // ).then (response => {
+    //     if (!response.ok) {
+    //         throw new Error(response.status);
+    //     }
+    // }).json()
 }
